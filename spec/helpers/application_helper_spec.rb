f7d02aa1d05c3bb510028da755f5f5315382e43a
anakin1359 <anakin1359@gmail.com>
2021-08-12 23:33:36 +0900

    applocation_helperのRspeテストを追加

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe "application_helper_spec" do
+    context "ページタイトルが存在する場合" do
+      it "「full_title」 、及び 「サークル運営サイト」の両方が表示されている" do
+        expect(full_title("title")).to eq "title | サークル運営サイト"
+      end
+    end
+
+    context "ページタイトルが存在しない場合" do
+      it "「サークル運営サイト」のみが表示されている" do
+        expect(full_title(" ")).to eq "サークル運営サイト"
+      end
+    end
+
+    context "引数の「page_title」がnilだった場合" do
+      it "「サークル運営サイト」のみが表示されている" do
+        expect(full_title(nil)).to eq "サークル運営サイト"
+      end
+    end
+  end
+end
