2d07acecbc260b12d615981d0f60a35776498eab
anakin1359 <anakin1359@gmail.com>
2021-08-13 08:36:14 +0900

    Userモデルのspecを追加（テストの雛形を作成）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User_modelのテスト" do
+    it "氏名, メールアドレス, パスワード(確認含む)が全て入力されている" do
+      user = User.new(
+        name:     "anakin",
+        email:    "anakin123@gmail.com",
+        # password: "password00",
+        # password_confirmation: "password00",
+      )
+      expect(user).to be_valid
+    end
+
+    # it "氏名未入力の場合は無効としている" do
+    #   user = User.new(name: nil)
+    #   user.valid?
+    #   expect(user.errors[:name]).to include("cant't be blank")
+    # end
+
+    # it "メールアドレス未入力の場合は無効としている" do
+    #   #
+    # end
+
+    # it "パスワード未入力の場合は無効としている" do
+    #   #
+    # end
+
+    # it "パスワード（確認）未入力の場合は無効としている" do
+    #   #
+    # end
+
+    # it "重複したメールアドレスの場合は無効としている" do
+    #   #
+    # end
+  end
 end
