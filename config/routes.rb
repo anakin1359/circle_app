d277178519cd93deab3b74cff8c7e1b129d3c636
anakin1359 <anakin1359@gmail.com>
2021-08-14 12:08:20 +0900

    deviceを用いたユーザ情報登録機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/contact', to: 'static_pages#contact'
 
-  resources :users, only: [:index, :show]
-  get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  # resources :users, only: [:index, :show]
+  # get  '/signup',  to: 'users#new'
+  # post '/signup',  to: 'users#create'
 end
