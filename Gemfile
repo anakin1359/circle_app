6ce760042422acacd75c7fca6b2a12911eacb781
anakin1359 <anakin1359@gmail.com>
2021-08-16 08:24:31 +0900

    faker機能を用いてsample_userをDBへ投入  - Gemfileへ"faker"を追加  - seeds.rbにsample_user30名を投入する設定を記述  - "rails db:seed"を実行

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
   gem 'rubocop', require: false
   gem 'rubocop-airbnb'
   gem 'bullet'
+  gem 'faker'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# このファイルには、データベースをデフォルト値でシードするために必要な、すべてのレコード作成が含まれているはずです。
+# このデータはその後、rails db:seedコマンドで読み込むことができます（またはdb:setupでデータベースと一緒に作成することもできます）。
 #
-# Examples:
+# 例を挙げます。
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name:  "anakin1359",
+  email: "anakin1359@gmail.com",
+  password:              "password01",
+  password_confirmation: "password01")
+
+50.times do |n|
+name  = Faker::Name.name
+email = "test-user-0#{n+1}@railstutorial.org"
+password = "password01"
+User.create!(name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password)
+end
