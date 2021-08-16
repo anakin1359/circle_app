4505b2bb79bd81374c8f5557116971cb76d5d6ad
anakin1359 <anakin1359@gmail.com>
2021-08-16 18:25:28 +0900

    ユーザの削除処理を実装  - adminユーザのみ削除ボタンが表示される制御を追加  - adminユーザ以外が削除処理を実行した場合エラーにする制御(admin_userメソッド)を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -7,30 +8,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # debugger
   end
 
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     flash[:success] = "Welcome to the Sample App!"
-  #     redirect_to @user
-  #   else
-  #     render 'new'
-  #   end
-  # end
-
-  def edit
-    @user = User.find(params[:id])
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:notice] = "ユーザを削除しました"
+    redirect_to users_path
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
+    flash[:alert] = "[エラー] 権限がありません"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/contact', to: 'static_pages#contact'
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :destroy]
   # get  '/signup',  to: 'users#new'
   # post '/signup',  to: 'users#create'
 end
