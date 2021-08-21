ee6e62d09afcda77386fbc8d5e31947bf0f635cc
anakin1359 <anakin1359@gmail.com>
2021-08-21 11:18:31 +0900

    登録済みのイベントを一覧で取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,18 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_user, only: :destroy
 
+  # ユーザ情報を一覧で表示
   def index
     @users = User.paginate(page: params[:page])
   end
 
+  # 登録したイベントを一覧で表示
   def show
     @user = User.find(params[:id])
+    @events = @user.events
   end
 
+  # ユーザ削除機能
   def destroy
     User.find(params[:id]).destroy
     flash[:notice] = "ユーザを削除しました"
@@ -23,6 +27,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password)
   end
 
+  # adminユーザでない場合はホーム画面にリダイレクトさせる
   def admin_user
     redirect_to root_path unless current_user.admin?
   end
