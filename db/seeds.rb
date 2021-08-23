dc977b9784b1bed9b6dbf4306949b1fb02939031
anakin1359 <anakin1359@gmail.com>
2021-08-23 17:58:00 +0900

    "rails db:seed"で再度管理者ユーザを含むテストエントリーデータを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,25 +28,39 @@
 # end
 
 # # テストイベント
+# 10.times do |n|
+#   title  = "test-event-0#{n+1}"
+#   introduction = "test-event-0#{n+1}_introduction"
+#   entry_count = n * 5
+#   event_price = n * 50
+#   start_date = Date.today.beginning_of_week
+#   end_date = Date.today.end_of_week
+#   address = "東京都千代田区神田駿河台1-1"
+#   event_icon = nil
+#   user_id = 1
+#   Event.create!(
+#     title:  title,
+#     introduction: introduction,
+#     entry_count: entry_count,
+#     event_price: event_price,
+#     start_date: start_date,
+#     end_date: end_date,
+#     address: address,
+#     event_icon: event_icon,
+#     user_id: user_id
+#   )
+# end
+
+# テストエントリー
 10.times do |n|
-  title  = "test-event-0#{n+1}"
-  introduction = "test-event-0#{n+1}_introduction"
-  entry_count = n * 5
-  event_price = n * 50
-  start_date = Date.today.beginning_of_week
-  end_date = Date.today.end_of_week
-  address = "東京都千代田区神田駿河台1-1"
-  event_icon = nil
-  user_id = 1
-  Event.create!(
-    title:  title,
-    introduction: introduction,
+  entry_count = 2
+  entry_price = n * 250
+  user_id = 10
+  event_id = 1
+  Entry.create!(
     entry_count: entry_count,
-    event_price: event_price,
-    start_date: start_date,
-    end_date: end_date,
-    address: address,
-    event_icon: event_icon,
-    user_id: user_id
+    entry_price: entry_price,
+    user_id: user_id,
+    event_id: event_id
   )
 end
