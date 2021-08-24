ff875d4fc1903725017c68e2a7158c575867eec9
anakin1359 <anakin1359@gmail.com>
2021-08-24 18:29:28 +0900

    jQueryの外部ファイル化（新たにcustom.jsを作成しそちらに記述）

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+  $('#entry_count').change(function() {
+  
+    var price = $('#event_price').val();
+    var count = $('#entry_count').val();
+  
+    var price_i = parseInt(price);
+    var count_i = parseInt(count);
+  
+    var result = price_i * count_i
+  
+    $('#entry_price').val(result)
+  });
+});
