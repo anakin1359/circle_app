de904ef46d93b2f09c4b3e442cc4ee55d4f270e9
anakin1359 <anakin1359@gmail.com>
2021-08-26 16:05:10 +0900

    画像アップロード機能の初期設定

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :entries, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :event_icon, ImageUploader
   with_options presence: true do
     validates :title, length: { maximum: 50 }
     validates :introduction, length: { maximum: 300 }
