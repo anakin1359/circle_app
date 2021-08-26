ddeba9ada35102f3f503d1850ae1dc2ac7cf8101
anakin1359 <anakin1359@gmail.com>
2021-08-26 14:45:08 +0900

    Gemfileに画像アップロード/編集機能のGemを追加  - carrierwave: 画像アップロード機能  - mini_magick: 画像編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/contact',       to: 'static_pages#contact'
-  # get  '/entries/index', to: 'entries#index'
 
   devise_for :users, controllers: {
     sessions:      'users/sessions',
