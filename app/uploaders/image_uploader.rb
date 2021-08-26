eec0e5a254d0a34435b8d281f4a1235d2d13c8f5
anakin1359 <anakin1359@gmail.com>
2021-08-26 19:24:50 +0900

    イベント編集画面(event/edit)にイベントアイコンが表示されるように設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  process resize_to_limit: [200, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -19,12 +20,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  # process scale: [200, 200]
   #
   # def scale(width, height)
   #   # do something
   # end
-  process resize_to_limit: [200, 200]
 
   # Create different versions of your uploaded files:
   # version :thumb do
