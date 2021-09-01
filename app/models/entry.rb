c6636c2c1e018150efd35e83bacb05fe2976d05d
anakin1359 <anakin1359@gmail.com>
2021-09-01 18:57:08 +0900

    予約レコードを降順で管理する設定に変更（entry.rbを編集）

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,7 @@
 class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  default_scope -> { order(created_at: :desc) }
   with_options presence: true do
     validates :entry_count
     validates :entry_price
