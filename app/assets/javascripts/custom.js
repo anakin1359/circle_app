051a6bb71d4c902788fddf2b5d53f1533164e935
anakin1359 <anakin1359@gmail.com>
2021-09-06 05:53:56 +0900

    イベント予約画面にGoogle.mapを表示

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,4 @@
+// ##########イベント予約時の料金自動計算処理 ##########
 
 $(document).on('turbolinks:load', function() {
   $('#entry_count').change(function() {
@@ -13,3 +14,36 @@ $(document).on('turbolinks:load', function() {
     $('#entry_price').val(result)
   });
 });
+
+
+// ########## Google.mapの表示 ##########
+var gMap = null;
+var gMarkerCenter = null;
+
+function initMap() {
+    // 登録されている住所の緯度/経度を利用して位置を特定
+    var e = document.getElementById('map');
+    var lat = e.getAttribute('data-lat-id');
+    var lng = e.getAttribute('data-lng-id');
+    var myLatLng = new google.maps.LatLng(lat, lng)
+    var mapOptions = {
+        center: myLatLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    // Google.map表示
+    gMap = new google.maps.Map(document.getElementById("map"), mapOptions);
+    gMarkerCenter = drawMarkerCenterInit(myLatLng);
+}
+
+
+// ########## Google.map Markerを生成 ##########
+function drawMarkerCenterInit(pos) {
+    var markerCenter = new google.maps.Marker({
+        position: pos,
+        map: gMap,
+        draggable: true
+    });
+    return markerCenter;
+}
